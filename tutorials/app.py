de4284afe66c0bfd32422d0c58a77eac0aea1b1c
GavrilS <gavr1lll@abv.bg>
2025-03-18 20:02:52 +0200

    Using streamlit magic commands to show app data

---
+++ tutorials/app.py
@@ -0,0 +1,15 @@
+"""
+First tutorial app - use data to create a table
+"""
+
+import streamlit as st
+import pandas as pd
+
+df = pd.DataFrame({
+    'first_column': [1, 2, 3, 4],
+    'second_column': [10, 20, 30, 40]
+})
+
+# Magic commands - every time streamlit sees a variable on its own line, it automatically writes
+# that to our app using 'st.write()'
+df
